a5d40268645158a390b9a4e6108b9404382c27cb
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-21 17:06:53 +0300

    Added 3rd as function

---
+++ TasksFunctions/Task_3.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE listStoreStaff(@store_name AS VARCHAR(255))
+AS
+BEGIN
+	SELECT
+		store_name,
+		sta.first_name + ' ' + sta.last_name AS staff_name,
+		m.first_name + ' ' + m.last_name AS manager_name
+	FROM
+		sales.stores sto
+	INNER JOIN sales.staffs sta ON sta.store_id = sto.store_id
+	LEFT JOIN sales.staffs m ON m.staff_id = sta.manager_id
+	WHERE 
+		store_name = @store_name
+END;
