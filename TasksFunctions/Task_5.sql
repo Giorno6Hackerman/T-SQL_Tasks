8b6f2c00fc2d79566059c0c6316aa37e8090afa2
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-21 17:19:06 +0300

    Added 5th as function

---
+++ TasksFunctions/Task_5.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE top3BrandsByProductsSold
+AS
+BEGIN
+	SELECT  TOP 3
+		brand_name,
+		SUM(quantity) AS products_sold
+	FROM 
+		sales.orders o
+	INNER JOIN sales.order_items oi ON oi.order_id = o.order_id
+	INNER JOIN production.products pp ON pp.product_id = oi.product_id
+	INNER JOIN production.brands pb ON pb.brand_id = pp.brand_id
+	WHERE
+		order_status = 4
+	GROUP BY 
+		brand_name
+	ORDER BY 
+		products_sold DESC
+END;
